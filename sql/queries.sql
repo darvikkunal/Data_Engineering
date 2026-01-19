b2f706508da7b06e81059c620ab5846afb8e2556
DarvikKunal Banda <123832825+darvikkunal@users.noreply.github.com>
2026-01-19 21:32:30 +0530

    Update sql/queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -3,6 +3,74 @@ select name from sys.databases;
 
 
 USE SalesDB;
-
 SELECT *
 FROM Sales.Customers;
+
+USE MyDatabase;
+-- Retrieve All customers
+SELECT *
+FROM MyDatabase.dbo.customers;
+-- Retrieve All order Data
+SELECT *
+FROM MyDatabase.dbo.orders;
+
+-- Retrieve each customer's name, country and score
+SELECT 
+    first_name,
+    country,
+    score
+FROM MyDatabase.dbo.customers;
+
+-- Retrieve customers with a score not equal to 0
+SELECT *
+FROM MyDatabase.dbo.customers
+WHERE score != 0;
+
+-- Retrieve customers with country Germany
+SELECT *
+FROM MyDatabase.dbo.customers
+WHERE country = 'Germany';
+
+-- Retrieve all customers and sort the results by the country in asc and highest score first 
+SELECT *
+FROM MyDatabase.dbo.customers
+ORDER BY country ASC , score DESC;
+
+--Find the total score for each country
+SELECT country , sum(score) as total_score
+FROM MyDatabase.dbo.customers
+GROUP BY country;
+
+-- Find the total score and total number of customers for each contry
+SELECT country, sum(score) as total_score , count(*) as no_of_customers
+FROM MyDatabase.dbo.customers
+GROUP BY country;
+
+-- Find the average score for each country considering only customers with a score not equal to 0
+-- And return only those countries with an average score greater than 430
+
+select country , avg(score) as avg_score
+FROM MyDatabase.dbo.customers
+WHERE score != 0
+GROUP BY country
+HAVING avg(score) > 430;
+
+-- Return unique list of countries
+select distinct(country) as unq_countries
+from MyDatabase.dbo.customers;
+
+-- Retrieve only 3 customers with HIghest score
+select TOP 3 *
+from MyDatabase.dbo.customers
+order by score desc;
+
+-- Retrieve the lowest 2 customers based on the score
+select TOP 2 *
+from MyDatabase.dbo.customers
+order by score ASC;
+
+-- Get the two most recent orders from orders table
+select TOP 2 *
+FROM MyDatabase.dbo.orders
+order by order_date desc;
+
