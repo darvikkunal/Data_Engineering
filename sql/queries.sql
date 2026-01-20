c4eb14a9c1e1db46d1e3b56526e7f4cbe2899413
DarvikKunal Banda <123832825+darvikkunal@users.noreply.github.com>
2026-01-20 11:39:44 +0530

    Update sql/queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -74,3 +74,29 @@ select TOP 2 *
 FROM MyDatabase.dbo.orders
 order by order_date desc;
 
+--DDL Commands--
+
+-- Create a new table called persons with columns: id, person_name, birth_date and phone
+
+CREATE TABLE persons (
+    id INT NOT NULL,
+    person_name VARCHAR(50) NOT NULL,
+    birth_date DATE,
+    phone VARCHAR(15) NOT NULL,
+    CONSTRAINT pk_persons PRIMARY KEY (id)
+);
+SELECT * FROM persons;
+
+-- Add a new column called email to the persons table
+
+ALTER TABLE persons
+ADD email VARCHAR(50) NOT NULL;
+
+-- Remove the column phone from the persons table
+
+ALTER TABLE persons
+DROP COLUMN phone; 
+
+-- Delete the table persons from the database
+
+DROP table persons;
