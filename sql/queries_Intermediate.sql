c7346c24d38104a73b5189a8693fd147865dae0b
DarvikKunal Banda <123832825+darvikkunal@users.noreply.github.com>
2026-01-28 12:07:32 +0530

    feat(sql): Add number function examples (ROUND, ABS)

--- sql/queries_Intermediate.sql
+++ sql/queries_Intermediate.sql
@@ -398,3 +398,14 @@ select
     first_name,
     SUBSTRING(TRIM(first_name),2,LEN(first_name)) sub_firstnm
 FROM MyDatabase.dbo.customers;
+
+---     Number Functions      ---
+SELECT
+3.516,
+ROUND(3.516,2) AS round_2,
+ROUND(3.516,1) AS round_1,
+ROUND(3.516,0) AS round_0;
+
+SELECT
+-3.516,
+ABS(-516);
