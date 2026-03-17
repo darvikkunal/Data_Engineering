77dabbf458d46ee2d5540548780fcd9c7d498c2e
DarvikKunal Banda <123832825+darvikkunal@users.noreply.github.com>
2026-03-17 11:37:29 +0530

    feat: format temperature output to two decimal places

--- DE_Drill/python/temperature.py
+++ DE_Drill/python/temperature.py
@@ -12,8 +12,8 @@ Kelvin = Celsius + 273.15
 Celsius = float(input("Enter temperature in Celsius : "))
 Fahrenheit = (Celsius * 9/5) + 32
 Kelvin = Celsius + 273.15
-print(f"The Fahrenheit temperature is : {Fahrenheit} F")
-print(f"The Kelvin temperature is : {Kelvin} K")
+print(f"The Fahrenheit temperature is : {Fahrenheit:.2f} F")
+print(f"The Kelvin temperature is : {Kelvin:.2f} K")
 
 
 
